b24808697b4b2c8ab5c282f97ecc524f148bfa07
terrotar <pedro_guedes93@hotmail.com>
2021-04-24 01:02:51 -0300

    Estrutura de arquivos modificada e adicionado algumas dependencias.

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,1 +1,14 @@
-import app
+# import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db_teste/sqlite_therapies.db"
+
+
+login_manager = LoginManager(app)
+
+
+db = SQLAlchemy(app)
--- back-end/app/config.py
+++
@@ -1,5 +0,0 @@
-
-# import main
-
-
-# ['SQLALCHEMY_CONNECTION_URI'] = "mysql://root:@192.168.15.2:3306/TCC"
