2ca50c69641c11f711e22155e21340ec677e55e8
terrotar <pedro_guedes93@hotmail.com>
2021-10-28 23:46:43 -0300

    deletado todos templates .html e inicio da criação a partir do 0.

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template, request
 
 # Instancia do Blueprint home
 home = Blueprint('home', __name__,
-                 template_folder="../../html_teste",
-                 static_folder="../../estaticos_teste")
+                 template_folder="../../templates",
+                 static_folder="../../estatic")
 
 
 # URL da homepage
 @home.route('/', methods=['GET'])
 def index():
     if(request.method == 'GET'):
-        return render_template('index_teste.html')
+        return render_template('index.html')
