f9c504d80179e93b63e4415c1c2695227004a9c6
terrotar <pedro_guedes93@hotmail.com>
2021-11-12 04:01:26 -0300

    Adicionado no index de usuario comum terapias e produtos naturais adicionados pelo administrador

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request
 
+from app.models.therapy import Therapy
+from app.models.product import Product
+
 
 # Instancia do Blueprint home
 home = Blueprint('home', __name__,
@@ -11,4 +14,8 @@ home = Blueprint('home', __name__,
 @home.route('/', methods=['GET'])
 def index():
     if(request.method == 'GET'):
-        return render_template('index.html')
+        all_therapies = Therapy.query.all()
+        all_products = Product.query.all()
+        return render_template('index.html',
+                               all_therapies=all_therapies,
+                               all_products=all_products)
