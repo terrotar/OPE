cc8e15e39ce0dbb066bcc7758e7bdb22dafe73ba
terrotar <pedro_guedes93@hotmail.com>
2021-12-18 00:12:18 -0300

    Fixed bug of user_type in admin pages.

--- app/config.py
+++ app/config.py
@@ -13,5 +13,5 @@ migrate = Migrate()
 
 UPLOAD_FOLDER = 'app/static/img'
 
-# app.config['SQLALCHEMY_DATABASE_URL'] = "postgresql://postgres:postgres@localhost/TCC"
+# DATABASE_URI = "postgresql://postgres:postgres@localhost/TCC"
 DATABASE_URI = 'postgresql://drzqvuvqbehtlc:975200bfaaca476937e61bfc1cf315e9f5a11e326c11c068ec2400754a8b9b0d@ec2-3-89-214-80.compute-1.amazonaws.com:5432/dep7ghagq8oere'
